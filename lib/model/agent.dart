4ee3de1801cb899b6f3ab74ea6e79d293ecd3df0
Nada <nada.nsiri@ecole.ensicaen.fr>
2024-02-29 01:11:11 +0100

    tortue circule

--- lib/model/agent.dart
+++ lib/model/agent.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 class TortoiseBrain {
-  String decideMove() {
+  String think(capteur) {
     List<String> actions = ['left', 'right', 'eat', 'drink','forward'];
     Random random = Random();
     return actions[random.nextInt(actions.length)];
